6610d17a31aa78daf22f6a8b9c4e5bd45e6c2a90
Nick Joyce <brawlyrox@gmail.com>
2018-11-25 18:42:05 +0000

    Tidy up comments

--- public/js/index.js
+++ public/js/index.js
@@ -16,12 +16,18 @@ socket.on('disconnect', () => {
 // Listen for a message from the server, an object will be retrieved
 socket.on('newMessage', (message) => {
     console.log('newMessage', message)
+    
+    // create the message to be displayed in a ol item
+    // create the HTML element
     const li = jQuery('<li></li>')
+    // append the text to the list tiem element
     li.text(`${message.from}: ${message.text}`)
-
+    // append the list item to the messages ol element
     jQuery('#messages').append(li)
 })
 
+// When the submit button is pressed send out the 'createMessage' event to socketio server with 
+// the value from the text box. 
 jQuery('#message-form').on('submit', (e) => {
     e.preventDefault()
 
